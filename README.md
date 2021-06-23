52c41050e9d9e5f543941e82c8d163126c47e6a5
louis-25 <20151577@vision.hoseo.edu>
2021-06-24 01:34:07 +0900

    이미지 추가

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # VueProject
 Vue를 연습하는 공간
 
+# Vue에서 사용될 변수
+data() { return( data:1 ) } 같이 넣어서 사용하자
+
+# Vue에서 사용될 함수
+methods 안에 작성하자
+함수 안에서 data 변수 사용할 경우 this로 호출하여 사용하자
 
 # 데이터바인딩
-{{ }} 안에 넣어주자
+{{ data }} 처럼 변수 사용가능
+
--- budongsan/src/App.vue
+++ budongsan/src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>{{ price1 }} 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
 
+  <div class="menu">
+    <a v-for="메뉴 in 메뉴들" :key="메뉴">{{ 메뉴 }}</a>
+  </div>
+  
+  <div v-for="product in products" :key="product">
+    <img :src="product.image" class="room-img" alt="">    
+    <h4 >{{ product.title }}</h4>
+    <p>{{ product.price }} 만원</p>
+    <button @click="product.신고수++">허위매물 신고버튼</button> <span>신고수 : {{product.신고수}}</span>
+  </div>
 
 </template>
 
@@ -17,10 +18,14 @@
 export default {
   name: 'App',
   data(){
-    return {
-      price1 : 80,
-      price2 : 90,
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    return {            
+      신고수 : 0,
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : [
+        {title : '역삼동원룸', price : 80, 신고수 : 0, image : require('./assets/room0.jpg')}, 
+        {title : '천호동원룸', price : 90, 신고수 : 0, image : require('./assets/room1.jpg')}, 
+        {title : '마포구원룸', price : 100, 신고수 : 0, image : require('./assets/room2.jpg')}
+        ],
     }
   },
   components: {
@@ -36,6 +41,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+  list-style: none;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
