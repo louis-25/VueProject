bb431a8ecaa93118790fda776897d3499749758c
louis-25 <20151577@vision.hoseo.edu>
2021-07-05 15:50:05 +0900

    vuestagram생성 및 데이터바인딩

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+    <div >
+        <Post :post=postData[i] v-for="(post,i) in postData" :key="post"/>
+    </div>
+</template>
+<script>
+import Post from './Post.vue'
+
+export default {
+    components: {
+        Post,
+    },
+    props: {
+        postData : Object,
+    },
+    
+}
+</script>
+<style></style>
